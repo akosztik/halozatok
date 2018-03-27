b6a9800dbf882357df5350cd539239974e1ee996
Kosztik <anett.kosztik@evosoft.com>
2018-03-27 17:42:44 +0200

    added bead

--- client.py
+++ client.py
@@ -5,18 +5,22 @@ client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_sock.connect(('localhost',12546))
 codebook = {'FLAG': '01111110', 'A': '10000001', 'B': '00001110', 'C': '10110011', 'D': '11100110', 'ESC': '01110000'}
 
-while True:
-    str1= codebook['FLAG']
-    i=2
-    while i <len(sys.argv):
-        print(sys.argv[i])
-        str1+=codebook[sys.argv[i]]
-        i+=1
-    str1+=codebook['ESC']    
-    print(str1)
-    client_sock.send(str1)
-    print(str1)
-    data = client_sock.recv(1024)
-    print(data)
-    print(data)
+type=sys.argv[1]
+str1=type + ':' + codebook['FLAG']
+print(str1)
+print("---------------")
+print(sys.argv)
+print("---------------")
+splitted=sys.argv[2].split(',')
+i=0
+while (i < len(splitted)):
+	print(splitted[i])
+	if ( splitted[i]=='FLAG' and type=='byte'):
+		str1+=codebook['ESC']
+	str1+=codebook[splitted[i]]
+	i+=1
+str1+=codebook['FLAG']
+print(str1)
+client_sock.send(str1.encode())
+print("SENDED")
 client_sock.close()
--- raktar.py
+++ raktar.py
@@ -2,6 +2,8 @@
 
 import socket
 import select
+import re
+
 codebook = {'FLAG': '01111110', 'A': '10000001', 'B': '00001110', 'C': '10110011', 'D': '11100110', 'ESC': '01110000'}
 
 server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -10,23 +12,37 @@ server_sock.listen(1)
 
 inputs = [server_sock]
 while True:
-    readable,writeable,ex = select.select(inputs,inputs,inputs)
-    for sock in readable:
-        if sock is server_sock:
-            client_sock, client_addr = server_sock.accept()
-            print("kapcsolat erkezett: ")
-            print(client_addr)
-            inputs.append(client_sock)
-        else:
-            data = sock.recv(1024)
-            if data:
-                print(data)
-                tipus = data.split(':')[0]
-                binaris = int(data.split(':')[1])
-                print("binaris tipusa: " + tipus)
-                print("binaris: " + binaris)
-                print(codebook)
-            else:
-                sock.close()
-                inputs.remove(sock)
+	readable,writeable,ex = select.select(inputs,inputs,inputs)
+	for sock in readable:
+		if sock is server_sock:
+			client_sock, client_addr = server_sock.accept()
+			print("kapcsolat erkezett: ")
+			print(client_addr)
+			inputs.append(client_sock)
+		else:
+			data = sock.recv(1024)
+			data = str(data)
+			data = data.replace("b", "", 1)
+			data = data.replace("'", "")
+			if data:
+				print(data)
+				tipus = data.split(':')
+				print(tipus)
+				print("binaris tipusa: " + tipus[0])
+				print("binaris: " + tipus[1])
+				splittedBinary=re.findall('........',tipus[1])
+				word=""
+				for bin in splittedBinary:
+					for key in codebook:
+						if codebook[key] == bin:
+							print(key)
+							word+=key
+				print(word)
+				word = word.replace("ESC", "")
+				word = word.replace("FLAG", "", 1)
+				word = word.replace("FLAG", "", len(word))
+				print(word)
+			else:
+				sock.close()
+				inputs.remove(sock)
 server_sock.close()
