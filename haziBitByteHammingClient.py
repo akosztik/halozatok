a22031c47363776f974268693ac30c7b9a3d44a6
akosztik <akosztik@gmail.com>
2018-04-15 19:58:21 +0200

    javítva 1.BEAD

--- haziBitByteHammingClient.py
+++ haziBitByteHammingClient.py
@@ -1,45 +1,52 @@
 import socket
 import sys
 
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_sock.connect(('localhost',12546))
-codebook = {'FLAG': '01111110', 'A': '10000001', 'B': '00001110', 'C': '10110011', 'D': '11100110', 'ESC': '01110000'}
-
-type=sys.argv[1]
-
 def byteConversion(splitted):
-    str1 = type + ':' + codebook['FLAG']
+    str1=''
     i = 0
     while (i < len(splitted)):
         if ((splitted[i] == 'FLAG' or splitted[i] == 'ESC') and type == 'byte'):
             str1 += codebook['ESC']
         str1 += codebook[splitted[i]]
         i += 1
-    str1 += codebook['FLAG']
     return str1
-
-splitted=sys.argv[2].split(',')
-str1 = byteConversion(splitted)
 
 def bitConversion(bitSplit):
-    str1 = ''
+    bitsor = ''
     i = 0
     counter = 0
-    while (i < len(bitSplit)):
-        str1= str1 + bitSplit[i]
-        if ( bitSplit[i] == '1'):
-            counter+=1
-        if (counter == 5):
-            str1 = str1 + '0'
+    length = len(bitSplit)
+    print(length)
+    while (i < length):
+        bitsor += bitSplit[i]
+        if ( bitSplit[i] ==  '1'):
+            counter += 1
+            if (counter == 5):
+                bitsor += '0'
+                counter = 0
+        else:
             counter = 0
-    return str1
+        i += 1
+    print('beszuras utan: ' + str(len(bitsor)))
+    return bitsor
+
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock.connect(('localhost',12546))
+codebook = {'FLAG': '01111110', 'A': '10000001', 'B': '00001110', 'C': '10110011', 'D': '11100110', 'ESC': '01110000'}
+
+type=sys.argv[1]
+splitted1 = sys.argv[2].split(',')
+print('splitted1: '+ str(splitted1))
+str1 = byteConversion(splitted1)
 
 if ( type=='bit'):
-    bitSplit = str1.split('')
-    str1 = bitConversion(str1)
+    bites = bitConversion(str1)
+    print('convertedBits: ' + str(bites))
+    str1=bites
 
-print(str1)
+str2 = type + ':' + codebook['FLAG'] + str1 + codebook['FLAG']
+print(str2)
 
-client_sock.send(str1.encode())
+client_sock.send(str2.encode())
 print("SENDED")
 client_sock.close()
--- server.py
+++ server.py
@@ -11,38 +11,81 @@ server_sock.bind(('localhost',12546))
 server_sock.listen(1)
 
 inputs = [server_sock]
+
+
+def removeBits(tipus):
+    str1 = ''
+    i = 0
+    counter = 0
+    while (i < len(tipus)):
+        if (counter == 5 and tipus[i]=='0'):
+            counter = 0
+        else:
+            if (tipus[i] == '1'):
+                counter += 1
+            else:
+                counter = 0
+            str1 = str1 + tipus[i]
+        i += 1
+    return str1
+
+
+def removeFrame(binary):
+    count = len(binary)
+    binary = binary[8:]
+    print(binary)
+    count = len(binary)
+    binary = binary[:count-8]
+    return binary
+
+
+def removeEsc(word):
+    word = word.replace("ESCESC", "ESC")
+    word = word.replace("ESCFLAG", "FLAG")
+    return word
+
+
+def byteConversion(binary):
+    splittedBinary = re.findall('........', binary)
+    word = ""
+    for bin in splittedBinary:
+        for key in codebook:
+            if codebook[key] == bin:
+                # print(key)
+                word += key
+    return word
+
+
 while True:
-	readable,writeable,ex = select.select(inputs,inputs,inputs)
-	for sock in readable:
-		if sock is server_sock:
-			client_sock, client_addr = server_sock.accept()
-			print("kapcsolat erkezett: ")
-			print(client_addr)
-			inputs.append(client_sock)
-		else:
-			data = sock.recv(1024)
-			data = str(data)
-			data = data.replace("b", "", 1)
-			data = data.replace("'", "")
-			if data:
-				print(data)
-				tipus = data.split(':')
-				#print(tipus)
-				print("binaris tipusa: " + tipus[0])
-				print("binaris: " + tipus[1])
-				splittedBinary=re.findall('........',tipus[1])
-				word=""
-				for bin in splittedBinary:
-					for key in codebook:
-						if codebook[key] == bin:
-							#print(key)
-							word+=key
-				#print(word)
-				word = word.replace("ESC", "")
-				word = word.replace("FLAG", "", 1)
-				word = word.replace("FLAG", "", len(word))
-				print("cliens altal kuldott szo: " +word)
-			else:
-				sock.close()
-				inputs.remove(sock)
+    readable,writeable,ex = select.select(inputs,inputs,inputs)
+    for sock in readable:
+        if sock is server_sock:
+            client_sock, client_addr = server_sock.accept()
+            print("kapcsolat erkezett: ")
+            print(client_addr)
+            inputs.append(client_sock)
+        else:
+            data = sock.recv(1024)
+            data = str(data)
+            data = data.replace("b", "", 1)
+            data = data.replace("'", "")
+            if data:
+                print(data)
+                tipus = data.split(':')
+                print(tipus)
+                binary = tipus[1]
+                type = tipus[0]
+                print("binaris tipusa: " + type)
+                print("binaris: " + binary)
+                binary = removeFrame(binary)
+                if (type=='bit'):
+                    binary=removeBits(binary)
+                    print("bitkonverzió után: " + binary)
+                word = byteConversion(binary)
+                if (type=='byte'):
+                    word = removeEsc(word)
+                print("cliens altal kuldott szo: " +word)
+            else:
+                sock.close()
+                inputs.remove(sock)
 server_sock.close()
