8e58a1bacbd617e5c9ae9ca5b3afa87568ebbc78
akosztik <akosztik@gmail.com>
2018-04-16 21:48:30 +0200

    CDMA 2.BEAD

---
+++ CDMAClient.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock.connect(('localhost',12546))
+chip = 'NNNN'
+name=sys.argv[1]
+str1 = 'I AM "' + name + '" .MY SEQ?'
+client_sock.send(str1.encode())
+data = client_sock.recv(1024).decode()
+print(data)
+if data == 'NNNN':
+    print("több kapcsolatot nem tudott fogadni a server! \n")
+    client_sock.close()
+else:
+    print("chipet kaptam: " + str(data))
+    chip = str(data)
+
+'''
+def cdmaEncoding(chipfogado, uzenet):
+    str1=''
+    return üzenet
+
+
+print("Add meg a cél clienst és az üzenetet! \n")
+data = raw_input()
+fogado = data.split(' ')[0]
+uzenet = data.split(' ')[1]
+client_sock.send(fogado)
+data = client_sock.recv(1024)
+chipfogado = str(data)
+encodoltUzenet = cdmaEncoding(chipfogado, uzenet)
+client_sock.send(encodoltUzenet)
+data = client_sock.recv(1024)
+print(data)
+'''
+
+client_sock.close()
---
+++ server2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import socket
+import select
+import re
+
+chipsequences = ['0001', '0010', '0100', '1000']
+
+server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_sock.bind(('localhost',12546))
+server_sock.listen(1)
+print("Waiting for connection...")
+inputs = [server_sock]
+clientAddrToChip = {}
+
+while True:
+    readable,writeable,ex = select.select(inputs,inputs,inputs)
+    for sock in readable:
+        if sock is server_sock:
+            client_sock, client_addr = server_sock.accept()
+            print( str(client_addr) + ' is connecting')
+            inputs.append(client_sock)
+
+        else:
+            data = sock.recv(1024).decode()
+            if data:
+                print(data)
+                name = data.split('"')[1]
+                print(name)
+                if len(chipsequences) != 0:
+                    clientAddrToChip[name] = chipsequences.pop()
+                    sock.send(clientAddrToChip[name].encode())
+                else:
+                    sock.send("NNNN".encode())
+                print(str(clientAddrToChip))
+            else:
+                sock.close()
+                inputs.remove(sock)
+server_sock.close()
+#https://www.youtube.com/watch?v=XJ81CuujwYE
+#https://www.youtube.com/watch?v=5plZGFd-cWc
