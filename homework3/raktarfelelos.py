2af97b33f88fdaf41d3026c627faacedd22817f9
akosztik <akosztik@gmail.com>
2018-05-07 14:10:20 +0200

    2-esért megvan a bead3

--- homework3/raktarfelelos.py
+++ homework3/raktarfelelos.py
@@ -3,7 +3,7 @@
 import socket
 import select
 
-keszlet = {'kocka':10,'kerék':20,'hangszóró':5, 'elem':30, 'kijelző':20}
+keszlet = {'kocka': 10, 'kerék': 20, 'hangszóró': 5, 'elem': 30, 'kijelző': 20}
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_sock.bind(('localhost', 12346))
@@ -22,21 +22,35 @@ while True:
             data = sock.recv(1024).decode()
             if data:
                 print(data)
+                kosar={}
                 szam = data.count(':')
                 lista = data.split(',')
                 i=0
                 van=True
-                while(i<=szam):
-                    item = lista[i].split(':')[0]
-                    mennyiseg = int(lista[i].split(':')[1])
-                    if (keszlet[item] < mennyiseg):
-                        van=False
 
-                if (van):
-                    sock.send('OK'.encode())
+                while i < szam:
+                    tmp = lista[i].split(':')[0]
+                    item = tmp.replace('"', '').replace('{', '').replace(' ', '')
+                    temp = lista[i].split(':')[1].replace('"', '').replace('}', '').replace(' ', '')
+                    mennyiseg = int(temp)
+                    kosar[item] = mennyiseg
+                    if keszlet[item] < mennyiseg:
+                        van = False
+                    i += 1
+                if van:
+                    for key in kosar:
+                        tmpMennyiseg = keszlet[key]-kosar[key]
+                        if tmpMennyiseg >= 0:
+                            keszlet[key]=tmpMennyiseg
+                        else:
+                            print("Hiba készletezéskor")
+                            van = False
+                    if van:
+                        sock.send('OK'.encode())
+                        print("vasarlas történt: " + str(kosar))
                 else:
                     sock.send('NEM OKE'.encode())
-                print(keszlet)
+                print("megmaradt készlet: " + str(keszlet))
             else:
                 sock.close()
                 inputs.remove(sock)
